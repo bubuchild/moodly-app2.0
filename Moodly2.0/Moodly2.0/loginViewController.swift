e9a9416ecb9bab4c211644ddf4614d268f11f1da
bubuchild <edvards.slepececs@gmail.com>
2020-11-05 22:59:29 +0000

    Setting up a Login/SignUp
    1) Login and SignUp segues change
    2) Setting up user email and password to be saved in Firebase database
    3) Testing Login and Sign up

--- Moodly2.0/Moodly2.0/loginViewController.swift
+++ Moodly2.0/Moodly2.0/loginViewController.swift
@@ -6,13 +6,31 @@
 //
 
 import UIKit
+import Firebase
 
 class loginViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginAction(_ sender: Any) {
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+           if error == nil{
+             self.performSegue(withIdentifier: "loginToHome", sender: self)
+                          }
+            else{
+             let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            
+              alertController.addAction(defaultAction)
+              self.present(alertController, animated: true, completion: nil)
+                 }
+        }
     }
     
 
--- Moodly2.0/Moodly2.0/signupViewController.swift
+++ Moodly2.0/Moodly2.0/signupViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class signupViewController: UIViewController {
-
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +31,30 @@ class signupViewController: UIViewController {
         print("Cancel button tapped")
                 
                 self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        if password.text != confirmPassword.text {
+        let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+                }
+        else{
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+         if error == nil {
+           self.performSegue(withIdentifier: "signupToHome", sender: self)
+                        }
+         else{
+           let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+           let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+               }
+            }
+        }
     }
     
 }
